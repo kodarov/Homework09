38a772e84d4f6851629e064e5e4a54d0d35ab22c
kodarov_salavat <kodarov@gmail.com>
2023-01-22 11:23:43 +0400

    Hw09\task04

--- src/Main.java
+++ src/Main.java
@@ -50,5 +50,9 @@ public class Main {
     }
     public static void task4(){
         System.out.println("Задание 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
